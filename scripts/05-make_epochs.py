e6dab0942a6873499876ee134b452eebecb051bf
jaeilepp <jaeilepp@student.jyu.fi>
2016-09-15 11:36:05 +0200

    Refactoring.

--- scripts/05-make_epochs.py
+++ scripts/05-make_epochs.py
@@ -9,10 +9,6 @@ from mne.preprocessing import create_ecg_epochs, read_ica
 
 from config import study_path, meg_dir, N_JOBS
 
-autoreject = True
-if autoreject:
-    from autoreject import GlobalAutoReject, validation_curve
-
 events_id = {
     'famous/first': 5,
     'famous/immediate': 6,
@@ -61,7 +57,7 @@ def run_epochs(subject_id):
             continue
 
         raw = mne.io.Raw(run_fname)
-        for idx, proj in enumerate(raw.info['projs']):
+        for idx, proj in enumerate(raw.info['projs']):  # find idx for EEG-ref
             if proj['kind'] == FIFF.FIFFV_MNE_PROJ_ITEM_EEG_AVREF:
                 proj_idx = idx
                 break
@@ -95,44 +91,12 @@ def run_epochs(subject_id):
         picks = mne.pick_types(raw.info, meg=True, eeg=True, stim=True,
                                eog=True)
 
-        if autoreject:
-            epochs = mne.Epochs(raw, events, events_id, tmin, tmax, proj=True,
-                                picks=picks, baseline=baseline, preload=True,
-                                reject=None)
-            reject = dict()
-            param_range = np.linspace(400e-15, 20000e-15, 30)
-            _, test_scores = validation_curve(
-                GlobalAutoReject(), epochs.copy().pick_types(meg='mag'),
-                y=None, param_name="thresh", param_range=param_range, cv=5,
-                n_jobs=N_JOBS)
+        # Read epochs
+        epochs = mne.Epochs(raw, events, events_id, tmin, tmax, proj=True,
+                            picks=picks, baseline=baseline, preload=True,
+                            reject=reject)
 
-            test_scores = -test_scores.mean(axis=1)
-            reject['mag'] = param_range[np.argmin(test_scores)]
-
-
-            param_range = np.linspace(400e-13, 20000e-13, 30)
-            _, test_scores = validation_curve(
-                GlobalAutoReject(), epochs.copy().pick_types(meg='grad'),
-                y=None, param_name="thresh", param_range=param_range, cv=5,
-                n_jobs=N_JOBS)
-            test_scores = -test_scores.mean(axis=1)
-            reject['grad'] = param_range[np.argmin(test_scores)]
-
-            param_range = np.linspace(20e-7, 400e-6, 30)
-            _, test_scores = validation_curve(
-                GlobalAutoReject(), epochs.copy().pick_types(meg=False,
-                                                             eeg=True),
-                y=None, param_name="thresh", param_range=param_range, cv=5,
-                n_jobs=N_JOBS)
-            test_scores = -test_scores.mean(axis=1)
-            reject['grad'] = param_range[np.argmin(test_scores)]
-            epochs.drop_bad(reject=reject)
-        else:
-            # Read epochs
-            epochs = mne.Epochs(raw, events, events_id, tmin, tmax, proj=True,
-                                picks=picks, baseline=baseline, preload=True,
-                                reject=reject)
-
+        # ICA
         ica_name = op.join(study_path, 'MEG', subject,
                            'run_%02d-ica.fif' % run)
         ica = read_ica(ica_name)
@@ -147,29 +111,6 @@ def run_epochs(subject_id):
 
     epochs = mne.epochs.concatenate_epochs(all_epochs)
     epochs.save(op.join(data_path, '%s-epo.fif' % subject))
-
-    evoked_famous = epochs['famous'].average()
-    evoked_scrambled = epochs['scrambled'].average()
-    evoked_unfamiliar = epochs['unfamiliar'].average()
-
-    # Simplify comment
-    evoked_famous.comment = 'famous'
-    evoked_scrambled.comment = 'scrambled'
-    evoked_unfamiliar.comment = 'unfamiliar'
-
-    contrast = mne.combine_evoked([evoked_famous, evoked_unfamiliar,
-                                   evoked_scrambled], weights=[0.5, 0.5, -1.])
-    contrast.comment = 'contrast'
-    faces = mne.combine_evoked([evoked_famous, evoked_unfamiliar])
-    faces.comment = 'faces'
-
-    mne.evoked.write_evokeds(op.join(data_path, '%s-ave.fif' % subject),
-                             [evoked_famous, evoked_scrambled,
-                              evoked_unfamiliar, contrast, faces])
-
-    # take care of noise cov
-    cov = mne.compute_covariance(epochs, tmin=tmin, tmax=0)
-    cov.save(op.join(data_path, '%s-cov.fif' % subject))
 
 
 parallel, run_func, _ = parallel_func(run_epochs, n_jobs=N_JOBS)

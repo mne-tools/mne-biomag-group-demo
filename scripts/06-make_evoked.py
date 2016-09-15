45a236afde81e043596cf9bac30f0c005a79b405
jaeilepp <jaeilepp@student.jyu.fi>
2016-09-15 11:47:19 +0200

    Evoked.

---
+++ scripts/06-make_evoked.py
@@ -0,0 +1,40 @@
+import os.path as op
+
+import mne
+from mne.parallel import parallel_func
+
+from config import meg_dir, N_JOBS
+
+
+def run_evoked(subject_id):
+    subject = "sub%03d" % subject_id
+    print("processing subject: %s" % subject)
+
+    data_path = op.join(meg_dir, subject)
+    epochs = mne.read_epochs(op.join(data_path, '%s-epo.fif' % subject))
+    evoked_famous = epochs['famous'].average()
+    evoked_scrambled = epochs['scrambled'].average()
+    evoked_unfamiliar = epochs['unfamiliar'].average()
+
+    # Simplify comment
+    evoked_famous.comment = 'famous'
+    evoked_scrambled.comment = 'scrambled'
+    evoked_unfamiliar.comment = 'unfamiliar'
+
+    contrast = mne.combine_evoked([evoked_famous, evoked_unfamiliar,
+                                   evoked_scrambled], weights=[0.5, 0.5, -1.])
+    contrast.comment = 'contrast'
+    faces = mne.combine_evoked([evoked_famous, evoked_unfamiliar])
+    faces.comment = 'faces'
+
+    mne.evoked.write_evokeds(op.join(data_path, '%s-ave.fif' % subject),
+                             [evoked_famous, evoked_scrambled,
+                              evoked_unfamiliar, contrast, faces])
+
+    # take care of noise cov
+    cov = mne.compute_covariance(epochs, tmin=tmin, tmax=0)
+    cov.save(op.join(data_path, '%s-cov.fif' % subject))
+
+
+parallel, run_func, _ = parallel_func(run_evoked, n_jobs=N_JOBS)
+parallel(run_func(subject_id) for subject_id in range(1, 20))

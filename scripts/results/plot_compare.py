a3efb42be65be04931b9462c3b06d0ff0c225e3b
jaeilepp <jaeilepp@student.jyu.fi>
2016-09-21 10:53:46 +0200

    plot_compare

--- scripts/results/plot_compare.py
+++ scripts/results/plot_compare.py
@@ -7,15 +7,18 @@ All the subjects compared.
 """
 
 import os.path as op
+import numpy as np
 
 import mne
+from library.config import study_path
 
+meg_dir = op.join(study_path, 'MEG')
 evokeds = list()
 for subject_id in range(1, 20):
     subject = "sub%03d" % subject_id
-    fname_in = op.join('..', '..', '..', 'MEG', subject,
-                       '%s-ave.fif' % subject)
+    fname_in = op.join(meg_dir, subject, '%s-ave.fif' % subject)
     evokeds.append(mne.read_evokeds(fname_in))
+times = np.arange(0.1, 0.26, 0.025)
 
 ###############################################################################
 # Evoked responses on EEG.
@@ -31,4 +34,5 @@ for idx, evoked in enumerate(evokeds):
 for idx, evoked in enumerate(evokeds):
     for cond in range(3):
         comm = evoked[cond].comment
-        evoked[cond].plot_topomap(title='Subject %s %s' % (idx + 1, comm))
+        evoked[cond].plot_topomap(times=times,
+                                  title='Subject %s %s' % (idx + 1, comm))

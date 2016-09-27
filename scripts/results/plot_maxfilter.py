3130586e2381ca79a01a21aa08e69f5a98f00577
Mainak Jas <mainak.jas@telecom-paristech.fr>
2016-09-27 17:48:34 +0200

    Remove tSSS because original paper used SSS only. Easy to compare

--- scripts/results/plot_maxfilter.py
+++ scripts/results/plot_maxfilter.py
@@ -15,19 +15,21 @@ from library.config import study_path, cal, ctc
 
 event_ids = [5, 6, 7]  # Famous faces
 
-subject = "sub002"
+subject = "sub003"
+bads = ['MEG1031', 'MEG1111', 'MEG2113']
+filter_params = dict(fir_window='hann', l_trans_bandwidth=0.5, phase='zero',
+                     h_trans_bandwidth='auto', filter_length='10s')
 
 raw_fname_in = op.join(study_path, 'ds117', subject, 'MEG', 'run_01_raw.fif')
 sss_fname_in = op.join(study_path, 'ds117', subject, 'MEG', 'run_01_sss.fif')
 
 ###############################################################################
 # First the filtered raw data.
-raw = mne.io.read_raw_fif(raw_fname_in, preload=True)
+raw = mne.io.read_raw_fif(raw_fname_in, preload=True, add_eeg_ref=False)
 
-raw.info['bads'] = ['MEG1031', 'MEG1111', 'MEG2032']  # set bads
+raw.info['bads'] = bads
 picks = mne.pick_types(raw.info, meg=True, exclude='bads')
-raw.filter(1, 40, fir_window='hann', l_trans_bandwidth=0.5, phase='zero',
-           h_trans_bandwidth='auto', filter_length='10s')
+raw.filter(1, 40, **filter_params)
 
 events = mne.find_events(raw, stim_channel='STI101', consecutive='increasing',
                          mask=4352, mask_type='not_and', min_duration=0.003,
@@ -36,17 +38,15 @@ evoked_before = Epochs(raw, events, event_id=event_ids, picks=picks).average()
 
 ###############################################################################
 # Then Maxfiltered and tSSS'd data.
-raw = mne.io.read_raw_fif(raw_fname_in, preload=True)
-raw_sss = mne.io.read_raw_fif(sss_fname_in, preload=True)
-raw.info['bads'] = ['MEG1031', 'MEG1111']
-raw_sss.info['bads'] = ['MEG1031', 'MEG1111']
+raw = mne.io.read_raw_fif(raw_fname_in, preload=True, add_eeg_ref=False)
+raw_sss = mne.io.read_raw_fif(sss_fname_in, preload=True, add_eeg_ref=False)
+raw.info['bads'] = bads
+raw_sss.info['bads'] = bads
 
-raw = maxwell_filter(raw, calibration=cal, cross_talk=ctc, st_duration=5.)
+raw = maxwell_filter(raw, calibration=cal, cross_talk=ctc)
 
-raw.filter(1, 40, fir_window='hann', l_trans_bandwidth=0.5, phase='zero',
-           h_trans_bandwidth='auto', filter_length='10s')
-raw_sss.filter(1, 40, fir_window='hann', l_trans_bandwidth=0.5, phase='zero',
-               h_trans_bandwidth='auto', filter_length='10s')
+raw.filter(1, 40, **filter_params)
+raw_sss.filter(1, 40, **filter_params)
 
 evoked_after = Epochs(raw, events, event_id=event_ids, picks=picks).average()
 evoked_sss = Epochs(raw_sss, events, event_id=event_ids, picks=picks).average()
@@ -54,12 +54,13 @@ evoked_sss = Epochs(raw_sss, events, event_id=event_ids, picks=picks).average()
 
 ###############################################################################
 # Plotting
-evoked_before.plot(spatial_colors=True,
+ylim = dict(grad=(-100, 100), mag=(-400, 400))
+evoked_before.plot(spatial_colors=True, ylim=ylim,
                    titles={'grad': 'Gradiometers before SSS',
                            'mag': 'Magnetometers before SSS'})
-evoked_after.plot(spatial_colors=True,
-                  titles={'grad': 'tSSS gradiometers',
-                          'mag': 'tSSS magnetometers'})
-evoked_sss.plot(spatial_colors=True,
-                titles={'grad': 'Maxfilter (tm) gradiometers',
-                        'mag': 'Maxfilter (tm) magnetometers'})
+evoked_after.plot(spatial_colors=True, ylim=ylim,
+                  titles={'grad': 'SSS gradiometers',
+                          'mag': 'SSS magnetometers'})
+evoked_sss.plot(spatial_colors=True, ylim=ylim,
+                titles={'grad': 'Maxfilter (TM) gradiometers',
+                        'mag': 'Maxfilter (TM) magnetometers'})

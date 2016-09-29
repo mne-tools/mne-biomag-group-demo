41a6c1f2745babb6c3bdd0969d12f25ac42fda95
jaeilepp <jaeilepp@student.jyu.fi>
2016-09-29 13:23:33 +0200

    Group stcs.

--- scripts/processing/14-group_average_source.py
+++ scripts/processing/14-group_average_source.py
@@ -15,6 +15,8 @@ from mne.minimum_norm import apply_inverse, read_inverse_operator
 from library.config import meg_dir, subjects_dir, spacing
 
 stcs = list()
+faces_stcs = list()
+scrambled_stcs = list()
 exclude = [1, 5, 16]  # Excluded subjects
 
 for run in range(1, 20):
@@ -27,7 +29,9 @@ for run in range(1, 20):
     evokeds = mne.read_evokeds(op.join(meg_dir, subject,
                                        '%s-ave.fif' % subject))
 
+    scrambled = evokeds[1]
     contrast = evokeds[3]
+    faces = evokeds[4]
     fname_inv = op.join(data_path, '%s-meg-%s-inv.fif' % (subject, spacing))
     inv = read_inverse_operator(fname_inv)
 
@@ -38,8 +42,24 @@ for run in range(1, 20):
     stcs.append(stc.morph(subject_from=subject, subject_to='fsaverage',
                           subjects_dir=subjects_dir))
 
+    stc = apply_inverse(faces, inv, lambda2, "dSPM", pick_ori=None)
+    faces_stcs.append(stc.morph(subject_from=subject, subject_to='fsaverage',
+                                subjects_dir=subjects_dir))
+
+    stc = apply_inverse(scrambled, inv, lambda2, "dSPM", pick_ori=None)
+    scrambled_stcs.append(stc.morph(subject_from=subject,
+                                    subject_to='fsaverage',
+                                    subjects_dir=subjects_dir))
+
 
 data = np.average([s.data for s in stcs], axis=0)
-
 stc = mne.SourceEstimate(data, stcs[0].vertices, stcs[0].tmin, stcs[0].tstep)
 stc.save(op.join(meg_dir, 'contrast-average'))
+
+data = np.average([s.data for s in faces_stcs], axis=0)
+stc = mne.SourceEstimate(data, stcs[0].vertices, stcs[0].tmin, stcs[0].tstep)
+stc.save(op.join(meg_dir, 'faces-average'))
+
+data = np.average([s.data for s in scrambled_stcs], axis=0)
+stc = mne.SourceEstimate(data, stcs[0].vertices, stcs[0].tmin, stcs[0].tstep)
+stc.save(op.join(meg_dir, 'scrambled-average'))

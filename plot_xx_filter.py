51732671dbab2b4e1cac21f586e00b05e2c411fa
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2016-09-29 12:11:33 +0200

    iter on plot_xx_filter

--- plot_xx_filter.py
+++ plot_xx_filter.py
@@ -2,15 +2,16 @@
 Select filters
 ==============
 
-Here we look at the choice of filters
+Here we look at the choice of filters both for low and high
+pass.
 """
 import numpy as np
 from scipy import signal
 import matplotlib.pyplot as plt
 
 sfreq = 1100.
-ylim = [-60, 10]  # for dB plots
-xlim = [0, 5.]
+ylim = [-40, 10]  # for dB plots
+xlim = dict(highpass=[0, 4.], lowpass=[35, 55])
 
 
 def box_off(ax):
@@ -20,37 +21,69 @@ def box_off(ax):
         ax.spines[key].set_visible(False)
 
 
-fig, axes = plt.subplots(1, 2, sharex=True, sharey=True, figsize=(12, 6))
-labels = ['v0.12', 'v0.13']
-for ax, label in zip(axes, labels):
-    for f_p in [1, 1.5, 2.5]:  # passband corner frequency
-        if label == 'v0.12':
-            transition_band = 0.5
-            filter_dur = 10
-            window = 'hann'
-        elif label == 'v0.13':
-            transition_band = np.minimum(np.maximum(0.25 * f_p, 2.), f_p)
-            filter_dur = 6.6 / transition_band  # sec
-            window = 'hamming'
-
+def design_filter(label, f_p, transition_band, filter_dur, window):
+    if label == 'highpass':
         f_s = f_p - transition_band
-        n = int(sfreq * filter_dur)
-        n += ~(n % 2)  # Type II filter can't have 0 attenuation at nyq
-
         # design the filter
         freq = [0., f_s, f_p, sfreq / 2.]
         gain = [0., 0., 1., 1.]
-        h = signal.firwin2(n, freq, gain, nyq=sfreq / 2., window=window)
+    else:
+        f_s = f_p + transition_band
+        # design the filter
+        freq = [0., f_p, f_s, sfreq / 2.]
+        gain = [1., 1., 0., 0.]
 
-        # plot freqency response
-        f, H = signal.freqz(h)
-        f *= sfreq / (2 * np.pi)
-        ax.plot(f, 20 * np.log10(np.abs(H)),
-                linewidth=2, zorder=4, label='$f_p$=%0.1f Hz' % f_p)
-        ax.set(xlim=xlim, ylim=ylim, xlabel='Frequency (Hz)')
-        ax.set_title('MNE default %s ' % label)
+    n = int(sfreq * filter_dur)
+    n += ~(n % 2)  # Type II filter can't have 0 attenuation at nyq
+
+    h = signal.firwin2(n, freq, gain, nyq=sfreq / 2., window=window)
+    return h
+
+
+def plot_filter(ax, h, xlim, label):
+    # plot freqency response
+    f, H = signal.freqz(h)
+    f *= sfreq / (2 * np.pi)
+    ax.plot(f, 20 * np.log10(np.abs(H)),
+            linewidth=2, zorder=4, label=label)
+    ax.set(xlim=xlim, ylim=ylim, xlabel='Frequency (Hz)')
     box_off(ax)
-    plt.legend(loc='lower right', ncol=1)
+
+
+fig, axes = plt.subplots(1, 2, sharey=True, figsize=(12, 4))
+f_ps = [1., 40.]
+labels = ['highpass', 'lowpass']
+for ax, f_p, label in zip(axes, f_ps, labels):
+    # MNE old defaults
+    transition_band = 0.5  # Hz
+    filter_dur = 10.  # seconds
+    window = 'hann'
+    h = design_filter(label, f_p, transition_band, filter_dur, window)
+    plot_filter(ax, h, xlim[label],
+                label='MNE (0.12)' + ('' if label == 'lowpass' else ' (Used)'))
+
+    # MNE new defaults
+    if label == "highpass":
+        transition_band = min(max(0.25 * f_p, 2.), f_p)  # Hz
+        ideal_gain = [0, 0, 1, 1]
+    else:
+        transition_band = min(max(0.25 * f_p, 2.), sfreq / 2. - f_p)  # Hz
+        ideal_gain = [1, 1, 0, 0]
+
+    filter_dur = 6.6 / transition_band  # sec
+    window = 'hamming'
+    h = design_filter(label, f_p, transition_band, filter_dur, window)
+    plot_filter(ax, h, xlim[label],
+                label='MNE (0.13)' + ('' if label == 'highpass' else ' (Used)'))
+
+    ideal_freq = [0, f_p, f_p, sfreq / 2.]
+    ideal_gain = np.array(ideal_gain, dtype=float)
+    ideal_gain[ideal_gain == 0.] = 10 ** (ylim[0] / 20)
+    ax.plot(ideal_freq, 20 * np.log10(ideal_gain), 'r--', alpha=0.5, linewidth=4,
+            zorder=3, label='Ideal')
+    ax.legend()
+    ax.set_title(label + " (cutoff %s Hz)" % f_p)
+
 axes[0].set_ylabel('Amplitude (dB)')
 plt.tight_layout()
 plt.show()

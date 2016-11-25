e87f11886ac988f5e894e43a05d31d8a7970cb51
jaeilepp <jaeilepp@gmail.com>
2016-11-25 15:29:59 +0100

    Exclude subject 019.

--- scripts/processing/11-make_watershed.py
+++ scripts/processing/11-make_watershed.py
@@ -9,7 +9,7 @@ import mne
 
 from library.config import subjects_dir
 
-for subject_id in range(1, 20):
+for subject_id in range(1, 19):  # subject 019 left out
     subject = "sub%03d" % subject_id
     mne.bem.make_watershed_bem(subject, subjects_dir=subjects_dir,
                                overwrite=True)

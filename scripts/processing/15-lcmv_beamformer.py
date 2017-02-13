eb70c8a35cb5318a1c3a7cf7d40702a44a4e7c41
jaeilepp <jaeilepp@gmail.com>
2017-02-13 15:24:23 +0100

    LCMV.

---
+++ scripts/processing/15-lcmv_beamformer.py
@@ -0,0 +1,38 @@
+"""
+LCMV beamformer
+===============
+
+Compute LCMV beamformer.
+"""
+
+import os.path as op
+
+import mne
+from mne.parallel import parallel_func
+from mne.beamformer import lcmv
+
+from library.config import meg_dir, spacing, N_JOBS
+
+
+def run_inverse(subject_id):
+    subject = "sub%03d" % subject_id
+    print("processing subject: %s" % subject)
+    data_path = op.join(meg_dir, subject)
+
+    fname_ave = op.join(data_path, '%s-ave.fif' % subject)
+    fname_cov = op.join(data_path, '%s-cov.fif' % subject)
+    fname_fwd = op.join(data_path, '%s-meg-%s-fwd.fif' % (subject, spacing))
+
+    epochs = mne.read_epochs(op.join(data_path, '%s-epo.fif' % subject),
+                             preload=False)
+    data_cov = mne.compute_covariance(epochs, tmin=0.03, tmax=0.3)
+    evoked = mne.read_evokeds(fname_ave, condition=[4])[0]
+    noise_cov = mne.read_cov(fname_cov)
+    forward = mne.read_forward_solution(fname_fwd, surf_ori=True)
+
+    stc = lcmv(evoked, forward, noise_cov, data_cov)
+    stc.save(op.join(data_path, 'mne_LCMV_inverse-contrast'))
+
+
+parallel, run_func, _ = parallel_func(run_inverse, n_jobs=N_JOBS)
+parallel(run_func(subject_id) for subject_id in range(1, 20))
---
+++ scripts/processing/16-group_average_lcmv.py
@@ -0,0 +1,34 @@
+"""
+Group average on LCMV beamformer
+================================
+
+Source estimates are computed for contrast between faces and scrambled and
+morphed to average brain,
+"""
+
+import os.path as op
+import numpy as np
+
+import mne
+
+from library.config import subjects_dir, meg_dir
+
+stcs = list()
+exclude = [1, 5, 16]  # Excluded subjects
+
+for run in range(1, 20):
+    if run in exclude:
+        continue
+    subject = "sub%03d" % run
+    print("processing subject: %s" % subject)
+    data_path = op.join(meg_dir, subject)
+
+    stc = mne.read_source_estimate(op.join(data_path,
+                                           'mne_LCMV_inverse-contrast'))
+    morphed = stc.morph(subject_from=subject, subject_to='fsaverage',
+                        subjects_dir=subjects_dir, grade=4)
+    stcs.append(morphed)
+
+data = np.average([s.data for s in stcs], axis=0)
+stc = mne.SourceEstimate(data, stcs[0].vertices, stcs[0].tmin, stcs[0].tstep)
+stc.save(op.join(meg_dir, 'contrast-average-lcmv'))

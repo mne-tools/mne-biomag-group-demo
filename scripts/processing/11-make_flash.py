d529c748cb9209db4daff6c3af9608087cde3126
jaeilepp <jaeilepp@gmail.com>
2017-06-21 14:14:25 +0200

    Cleaning.

--- scripts/processing/11-make_flash.py
+++ scripts/processing/11-make_flash.py
@@ -2,14 +2,14 @@
 Create BEM surfaces
 ===================
 
-Create BEM surfaces using watershed algorithm.
+Create BEM surfaces using flash images.
 """
 
 import mne
 
 from library.config import subjects_dir
 
-for subject_id in range(1, 20):  # subject 019 left out
+for subject_id in range(1, 20):
     subject = "sub%03d" % subject_id
     mne.bem.make_flash_bem(subject, subjects_dir=subjects_dir, overwrite=True,
                            show=False)
